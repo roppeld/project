624acc669f99ad62a13fbb0f5723cd0861d05fe7
Margo <roppeldmargo@gmail.com>
2021-01-04 17:55:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert("Hello"); 
+
+// const result = confirm("Neo, wake up! Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть уже 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Вашa фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`http://someurl.com/${category}/5`); 
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
