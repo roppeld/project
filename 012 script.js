9e8a735c77908a647c6e2de37515d861826819f9
Margo <roppeldmargo@gmail.com>
2021-01-06 23:25:47 +0200

    homeWork mod 2

--- 012 script.js
+++ 012 script.js
@@ -27,3 +27,11 @@
 
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
