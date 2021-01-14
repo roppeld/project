89e5403eb8f609a72f33060f3d130cbfd8e03aa1
Margo <roppeldmargo@gmail.com>
2021-01-14 22:40:44 +0200

    fixed

--- 015 script.js
+++ 015 script.js
@@ -31,25 +31,26 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+for(let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
           b = prompt("На сколько оцените его?", "");
 
-    if (a != null && b != null && a != "" && b != "" && a < 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         console.log("done");
         personalMovieDB.movies[a] = b; 
     } else {
         console.log("error");
         i--;
     }
+   
  
 }
 
-if (personalMovieDB.count < 10) {
+if(personalMovieDB.count < 10) {
    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
+} else if(personalMovieDB.count >= 30) {
     console.log("Вы киноман");
 } else {
     console.log("Произошла ошибка");
