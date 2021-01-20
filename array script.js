abfad70ddaa67d47f6bfd7f4570b0377dac458f9
Margo <roppeldmargo@gmail.com>
2021-01-20 20:12:34 +0200

    already

--- array script.js
+++ array script.js
@@ -1,10 +1,12 @@
 "use strict";
 
- const arr = [1, 2, 3, 6, 8];
+  const arr = [2, 1, 5, 3, 6, 8];
+  arr.sort();
+  console.log(arr);
 
- arr.forEach(function(item, i, arr) { // колбэк функция будет для каждого 
-     console.log(`${i}: ${item} внутри массива ${arr}`); // элемента массива
- });
+//  arr.forEach(function(item, i, arr) { // колбэк функция будет для каждого 
+//      console.log(`${i}: ${item} внутри массива ${arr}`); // элемента массива
+//  });
 
 // arr.pop();
 // arr.push(10);
@@ -19,3 +21,8 @@
 //     console.log(value);
 // }
 
+const str = prompt('','');
+const products = str.split(", ");
+products.sort();
+// console.log(products);
+console.log(products.join(' - '));
