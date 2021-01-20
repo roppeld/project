59e3a4c2af24ed26a7149a4b9af460e8ae8e1f9c
Margo <roppeldmargo@gmail.com>
2021-01-20 20:18:29 +0200

    done

--- 015 script.js
+++ 015 script.js
@@ -91,14 +91,14 @@ writeYourGenres();
 showMyBD();
 
 //функции callback 
-// function learnJS(lang, callback) {
-//     console.log(`Я учу: ${lang}`);
-//     callback();
-// }
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
 
-// learnJS('JavaScript', function() {
-//     console.log('Я прошёл этот урок!');
-// });
+learnJS('JavaScript', function() {
+    console.log('Я прошёл этот урок!');
+});
 
 function learnJS(lang, callback) {
     console.log(`Я учу: ${lang}`);
