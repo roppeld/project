84267a122a045bc7cb7b811d613466cde5704ed8
Margo <roppeldmargo@gmail.com>
2021-01-20 23:10:35 +0200

    spread lesson

--- lesson copy script.js
+++ lesson copy script.js
@@ -59,3 +59,19 @@ const newArray = oldArray.slice();
 newArray[1] = 'sfsfsdfsdf';
 console.log(oldArray);
 console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook', 'github'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 4, 7];
+
+log(...num);
