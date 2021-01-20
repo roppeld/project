eb79b0bbd1771014653b03818b912a1c1d72f710
Margo <roppeldmargo@gmail.com>
2021-01-20 20:13:50 +0200

    all code

--- array script.js
+++ array script.js
@@ -1,28 +1,28 @@
 "use strict";
 
-  const arr = [2, 1, 5, 3, 6, 8];
+  const arr = [26, 10, 5, 3, 6, 8];
   arr.sort();
   console.log(arr);
 
-//  arr.forEach(function(item, i, arr) { // колбэк функция будет для каждого 
-//      console.log(`${i}: ${item} внутри массива ${arr}`); // элемента массива
-//  });
+ arr.forEach(function(item, i, arr) { // колбэк функция будет для каждого 
+     console.log(`${i}: ${item} внутри массива ${arr}`); // элемента массива
+ });
 
-// arr.pop();
-// arr.push(10);
+arr.pop();
+arr.push(10);
 
-// //console.log(arr); 
+console.log(arr); 
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+for (let value of arr) {
+    console.log(value);
+}
 
 const str = prompt('','');
 const products = str.split(", ");
 products.sort();
-// console.log(products);
+console.log(products);
 console.log(products.join(' - '));
