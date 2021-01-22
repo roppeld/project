b2c569a3f6af41fbd22977c332996f9e3a3913ef
Margo <roppeldmargo@gmail.com>
2021-01-22 22:26:05 +0200

    mod 3.0

--- Dinamic type.js
+++ Dinamic type.js
@@ -21,3 +21,27 @@ console.log(typeof(Number('4')));
 //2) унарный плюс
 
 console.log(typeof(+'5'));
+
+// 3)
+
+console.log(parseInt("15px", 10));
+
+// To Boolean 
+//пустота или ничего, всегда будет ложью
+// 0, '', null, undefined (что-то но без значения), NaN (специальное
+// значение числового типа) и всё это будет превращаться в false
+
+//правдой в логическом контексте будут пустые массивы, пустые объекты
+// строки  и все остальное будет правдой в логическом контексте true
+// 1)
+let swither = null; // заработает с swither = 1
+
+if (swither) { //работать не будет, потому что null - false, а условие
+    console.log('Working...'); // нуждается в true
+}
+
+//2) 
+console.log(typeof(Boolean('4'))); //same, but for boolean
+
+//3) 
+console.log(typeof(!!"44444")); // boolean type, very rare method
