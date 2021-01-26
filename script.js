6e8d19ac6041e1e00f17a3de2d28902cafe06ed4
Margo <roppeldmargo@gmail.com>
2021-01-26 20:40:38 +0200

    one more

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+
+console.log(btns);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
