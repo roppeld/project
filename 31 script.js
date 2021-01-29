bbd8ed2db754e7e741c0d826925e86b61f380cd4
Margo <roppeldmargo@gmail.com>
2021-01-29 22:04:26 +0200

    29.01.21

--- 31 script.js
+++ 31 script.js
@@ -1,5 +1,38 @@
-const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button');
 
-btn.onclick = function() {
-    alert('Click');
+
+// btn.onclick = function() {
+//     alert('Click');
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+
+// btn.addEventListener('click', () => {
+//     alert('Second click');
+// });
+
+// btn.addEventListener('mouseenter', () => {
+//     console.log('Hi'); 
+// });
+
+// btn.addEventListener('click', (e) => {
+//     e.target.remove();
+// });
+
+ //let i = 0;
+const deleteElement = (e) => {
+    console.log(e.target);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
 };
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+// btn.addEventListener('click', deleteElement);
+
