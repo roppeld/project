4f6d58fbbd72b1deafb1acc545d3bb749c89d419
Margo <roppeldmargo@gmail.com>
2021-01-31 23:13:07 +0200

    032 mod 3.0

--- 032 script.js
+++ 032 script.js
@@ -5,12 +5,20 @@
 // console.log(document.body.firstChild);
 // console.log(document.body.lastChild);
 
-console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
 
-// console.log(document.querySelector('[data-current = "3"]'));
-console.log(document.querySelector('[data-current = "3"]').previousSibling);
-console.log(document.querySelector('[data-current = "3"]').nextSibling);
-console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
-console.log(document.querySelector('[data-current = "3"]')
-.previousElementSibling);
+// // console.log(document.querySelector('[data-current = "3"]'));
+// console.log(document.querySelector('[data-current = "3"]').previousSibling);
+// console.log(document.querySelector('[data-current = "3"]').nextSibling);
+// console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current = "3"]')
+// .previousElementSibling);
 
+
+// способ перебрать в ноде ребенка элементы, исключив текстовые ноды
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
