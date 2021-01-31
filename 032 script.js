30915bbf1cd1dbc3140766c71c088da320c1c4d6
Margo <roppeldmargo@gmail.com>
2021-01-31 19:24:08 +0200

    032 lesson 2.0

--- 032 script.js
+++ 032 script.js
@@ -1,2 +1,16 @@
 // console.log(document.body);
-console.log(document.head);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current = "3"]'));
+console.log(document.querySelector('[data-current = "3"]').previousSibling);
+console.log(document.querySelector('[data-current = "3"]').nextSibling);
+console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+console.log(document.querySelector('[data-current = "3"]')
+.previousElementSibling);
+
