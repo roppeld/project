feaf81b5d99c787e5fd58cf21402f2e6637d586d
Margo <roppeldmargo@gmail.com>
2021-02-09 20:48:26 +0200

    mod file css and js

--- 033 script.js
+++ 033 script.js
@@ -50,13 +50,20 @@ document.addEventListener('DOMContentLoaded', () => {
     addForm.addEventListener('submit', (event) => {
        event.preventDefault();
 
-       const newFilm = addInput.value;
+       let newFilm = addInput.value;
        const favorite = checkbox.checked;
 
-       movieDB.movies.push(newFilm);
-       sortArr(movieDB.movies);
+        if (newFilm) {
 
-       createFilmsList(movieDB.movies, movieList);
+        if (newFilm.length >= 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`;   
+        }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+ 
+            createFilmsList(movieDB.movies, movieList);
+        }
 
        event.target.reset();
     });
