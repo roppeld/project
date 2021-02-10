f0b1d8752b3effb4e4215d81c4a2f1760e82cb72
Margo <roppeldmargo@gmail.com>
2021-02-10 22:21:30 +0200

    dynamic type injection 2.0 all done

--- 035 script.js
+++ 035 script.js
@@ -3,6 +3,11 @@
 const p = document.querySelectorAll('p');
 console.log(p);
 
-const script = document.createElement('script');
-script.src = "035.2 script.js";
-document.body.append(script);
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = "035.2 script.js";
+    script.async = src;
+    document.body.append(script);
+}
+
+loadScript("035.2 script.js");
