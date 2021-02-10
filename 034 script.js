5ac26830016a3417b71d7778e7638355afe2ed4f
Margo <roppeldmargo@gmail.com>
2021-02-10 19:20:09 +0200

    lesson done and one more start

--- 034 script.js
+++ 034 script.js
@@ -3,4 +3,29 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('Start');
+        // console.log(e.touches); 
+        console.log(e.targetTouches);
+    });
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX); //обращаясь к первому пальцу
+                                              //на конкретном элементе просим
+                                              //у него координаты
+    });
+    
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('End');
+    // });
+
+
 }); 
